edf7e43a262694da556d5d2a987ecec342b3aad3
npofsi koi | N/P硅 <npofsi@outlook.com>
2025-09-21 23:35:21 +0900

    feat: add friends widget

---
+++ .astro/content-assets.mjs
@@ -0,0 +1,4 @@
+
+import __ASTRO_IMAGE_IMPORT_ZwhNgE from "simulink_block.png?astroContentImageFlag=&importer=src%2Fcontent%2Fposts%2Fprogrammatically-simulink-with-simscape%2Findex.md";
+export default new Map([["simulink_block.png?astroContentImageFlag=&importer=src%2Fcontent%2Fposts%2Fprogrammatically-simulink-with-simscape%2Findex.md", __ASTRO_IMAGE_IMPORT_ZwhNgE]]);
+		
